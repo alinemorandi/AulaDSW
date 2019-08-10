cd3b6b9c559b42cfd3c3c5e117009501b1e6e34d
Aline Morandi <alinemorandi90@gmail.com>
2019-08-10 14:37:23 -0300

    criando projeto

---
+++ src/main/java/com/example/AulaDSW/AulaDswApplication.java
@@ -0,0 +1,13 @@
+package com.example.AulaDSW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaDswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaDswApplication.class, args);
+	}
+
+}
