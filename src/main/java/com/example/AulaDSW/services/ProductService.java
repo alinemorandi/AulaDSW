5890dabc07051b63418e05ac7013c940d2b93e39
Aline Morandi <alinemorandi90@gmail.com>
2019-08-26 09:23:52 -0300

    Product

---
+++ src/main/java/com/example/AulaDSW/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.example.AulaDSW.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.AulaDSW.entities.Product;
+import com.example.AulaDSW.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
