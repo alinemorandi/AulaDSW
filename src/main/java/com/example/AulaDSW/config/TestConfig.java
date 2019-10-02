3f9ccbf285010c4802d2b9c116779f6637576a55
Aline Morandi <alinemorandi90@gmail.com>
2019-10-02 10:14:08 -0300

    Data model

--- src/main/java/com/example/AulaDSW/config/TestConfig.java
+++ src/main/java/com/example/AulaDSW/config/TestConfig.java
@@ -13,12 +13,14 @@ import com.example.AulaDSW.entities.Order;
 import com.example.AulaDSW.entities.OrderItem;
 import com.example.AulaDSW.entities.Payment;
 import com.example.AulaDSW.entities.Product;
+import com.example.AulaDSW.entities.Role;
 import com.example.AulaDSW.entities.User;
 import com.example.AulaDSW.entities.enums.OrderStatus;
 import com.example.AulaDSW.repositories.CategoryRepository;
 import com.example.AulaDSW.repositories.OrderItemRepository;
 import com.example.AulaDSW.repositories.OrderRepository;
 import com.example.AulaDSW.repositories.ProductRepository;
+import com.example.AulaDSW.repositories.RoleRepository;
 import com.example.AulaDSW.repositories.UserRepository;
 
 @Configuration
@@ -40,6 +42,9 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private OrderItemRepository orderItemRepository;
 
+	@Autowired
+	private RoleRepository roleRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -68,11 +73,24 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
+		userRepository.saveAll(Arrays.asList(u1, u2));
+		
+		Role r1 = new Role (null, "ROLE_CLIENT");
+		Role r2 = new Role (null, "ROLE_ADMIN");
+		
+		roleRepository.saveAll(Arrays.asList(r1,r2)); 
+		
+		u1.getRoles().add(r1);
+		u2.getRoles().add(r1);
+		u2.getRoles().add(r2);
+		
+		userRepository.saveAll(Arrays.asList(u1,u2));
+		
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
-		userRepository.saveAll(Arrays.asList(u1, u2));
+		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
 		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
---
+++ src/main/java/com/example/AulaDSW/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.AulaDSW.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.AulaDSW.entities.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
