fa252d60cba84c88171e01824c17d76da3bf3465
Aline Morandi <alinemorandi90@gmail.com>
2019-10-05 14:57:40 -0300

    Order GET endpoints PART 2

--- src/main/java/com/example/AulaDSW/services/OrderService.java
+++ src/main/java/com/example/AulaDSW/services/OrderService.java
@@ -15,26 +15,29 @@ import com.example.AulaDSW.entities.Order;
 import com.example.AulaDSW.entities.OrderItem;
 import com.example.AulaDSW.entities.User;
 import com.example.AulaDSW.repositories.OrderRepository;
+import com.example.AulaDSW.repositories.UserRepository;
 import com.example.AulaDSW.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class OrderService {
 	
 	@Autowired
-	private OrderRepository repository;
+	private OrderRepository orderRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
 	
 	@Autowired
 	private AuthService authService;
 	
 	public List<OrderDTO> findAll(){
 
-		List<Order> list = repository.findAll();
-
+		List<Order> list = orderRepository.findAll();
 		return list.stream().map(e -> new OrderDTO(e)).collect(Collectors.toList());
 	}
 	
 	public OrderDTO  findById(Long id) {
-		Optional<Order> obj = repository.findById(id);
+		Optional<Order> obj = orderRepository.findById(id);
 		Order entity = obj.orElseThrow(() -> new ResourceNotFoundException(id));
 		authService.validadeOwnOrderOrAdmin(entity);
 		return new OrderDTO(entity);
@@ -42,16 +45,23 @@ public class OrderService {
 	
 	public List<OrderDTO> findByClient() {
 		User client = authService.authenticated();
-		List<Order> list = repository.findByClient(client);
+		List<Order> list = orderRepository.findByClient(client);
 		return list.stream().map(e -> new OrderDTO(e)).collect(Collectors.toList());
 	}
 
 	@Transactional(readOnly = true)
 	public List<OrderItemDTO> findItems(Long id) {
-		Order order = repository.getOne(id);
+		Order order = orderRepository.getOne(id);
 		authService.validadeOwnOrderOrAdmin(order);
 		Set<OrderItem> set = order.getItems();
 		return set.stream().map(e -> new OrderItemDTO(e)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findByClientId(Long clientId) {
+		User client = userRepository.getOne(clientId);
+		List<Order> list = orderRepository.findByClient(client);
+		return list.stream().map(e -> new OrderDTO(e)).collect(Collectors.toList());
 	}
 
 	
